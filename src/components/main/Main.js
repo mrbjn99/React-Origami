4738d6d1a42af02d832df5d706cf44561ae43c96
mrbjn99 <mrbjn99@gmail.com>
2021-05-24 21:23:13 +0700

    version 0.5

---
+++ src/components/main/Main.js
@@ -0,0 +1,22 @@
+import React, { Component} from "react";
+
+class Main extends Component {
+    render() {
+        return(
+            <div class="App_Container" style={{ position: "fixed", top :"9rem" , right: "45rem"}}>
+                <main class="Main">
+                    <h1>Soooooooooooome Heading</h1>
+                </main>
+            </div>
+        
+        
+        
+        
+        );
+    
+    }
+        
+    
+    
+}
+export default Main;
